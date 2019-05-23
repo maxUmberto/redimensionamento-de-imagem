8f9d17b2d4fd2309e20a72417b8cd4694dc56868
Max <max.umberto97@gmail.com>
2019-05-23 20:59:31 -0300

    Redimensionando as imagens

--- redimensiona.php
+++ redimensiona.php
@@ -12,17 +12,23 @@ function valida_arquivos($arquivos){
   $erro = '';
   if(empty($arquivos['tmp_name'][0])){
     $erro .= 'Nenhum arquivo selecionado';
-  }
+  } else {
 
-  for($i = 0; $i < count($arquivos['tmp_name']); $i++){
-    if($arquivos['type'][$i] != 'image/jpeg' && $arquivos['type'][$i] != 'image/png'){
-      $erro .= 'Arquivo: "'.$arquivos['name'][$i].'" tem formato inválido. Envie somente imagens em formato PNG ou JPEG/JPG<br>';
-    } else if($arquivos['size'][$i] >= 4194304){
-      $erro .= 'O Arquivo: "'.$arquivos['name'][$i].'" é muito grande. Tamanho máximo de 4Mb<br>';
+    for($i = 0; $i < count($arquivos['tmp_name']); $i++){
+      if($arquivos['type'][$i] != 'image/jpeg' && $arquivos['type'][$i] != 'image/png'){
+        $erro .= 'Arquivo: "'.$arquivos['name'][$i].'" tem formato inválido. Envie somente imagens em formato PNG ou JPEG/JPG<br>';
+      } else if($arquivos['size'][$i] >= 4194304){
+        $erro .= 'O Arquivo: "'.$arquivos['name'][$i].'" é muito grande. Tamanho máximo de 4Mb<br>';
+      }
     }
+
   }
 
   return $erro;
+}
+
+function redimensiona($imagem_final, $imagem_original,$largura, $altura, $largura_original, $altura_original){
+  return imagecopyresampled($imagem_final, $imagem_original, 0, 0, 0, 0, $largura, $altura, $largura_original, $altura_original);
 }
 
 $erros = '';
@@ -30,7 +36,41 @@ $erros .= valida($_POST['largura'], 'Largura');
 $erros .= valida($_POST['altura'], 'Altura');
 $erros .= valida_arquivos($_FILES['arquivo']);
 
-echo $erros;
+if($erros != ''){
+  echo $erros;
+  echo '<br><br><a href="index.php"><- Voltar</a>';
+  exit();
+}
 
+$largura = $_POST['largura'];
+$altura = $_POST['altura'];
+$arquivos = $_FILES['arquivo'];
+
+for($i = 0; $i < count($arquivos['tmp_name']); $i++){
+  list($largura_original, $altura_original) = getImageSize($arquivos['tmp_name'][$i]);
+
+  $proporcao = $largura_original / $altura_original;
+
+  if($largura / $altura > $proporcao){
+    $largura = $altura * $proporcao;
+  } else{
+    $altura = $largura / $proporcao;
+  }
+
+  $imagem_final = imagecreatetruecolor($largura, $altura);
+
+  if($arquivos['type'][$i] == 'image/jpeg'){
+    $imagem_original = imagecreatefromjpeg($arquivos['tmp_name'][$i]);
+    redimensiona($imagem_final, $imagem_original,$largura, $altura, $largura_original, $altura_original);
+    imagejpeg($imagem_final, 'imagens/'.$i.'.jpeg',100);
+    echo '<img src="imagens/'.$i.'.jpeg"><br><br>';
+  } else {
+    $imagem_original = imagecreatefrompng($arquivos['tmp_name'][$i]);
+    redimensiona($imagem_final, $imagem_original,$largura, $altura, $largura_original, $altura_original);
+    imagepng($imagem_final, 'imagens/'.$i.'.png');
+    echo '<img src="imagens/'.$i.'.jpeg"><br><br>';
+  }
+
+}
 
 ?>
