427ef94780d008237b6f58f5bf26de74221cb55c
Max <max.umberto97@gmail.com>
2019-05-24 16:48:38 -0300

    Commit da depressão, codando só pra afastar a bad

--- redimensiona.php
+++ redimensiona.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'validacao.php';
+require_once 'cabecalho.html';
 
 function redimensiona($imagem_final, $imagem_original,$largura, $altura, $largura_original, $altura_original, $i, $formato){
 
@@ -25,6 +26,11 @@ $altura = $_POST['altura'];
 $manter_proporcao = $_POST['proporcao'];
 $arquivos = $_FILES['arquivo'];
 
+?>
+<div class="container">
+  <div class="row">
+<?php
+
 for($i = 0; $i < count($arquivos['tmp_name']); $i++){
   list($largura_original, $altura_original) = getImageSize($arquivos['tmp_name'][$i]);
 
@@ -40,22 +46,33 @@ for($i = 0; $i < count($arquivos['tmp_name']); $i++){
 
   $imagem_final = imagecreatetruecolor($largura, $altura);
 
+  ?>
+      <div class="col-md-8 offset-md-2">
+        <div class="text-center">
+  <?php
   if($arquivos['type'][$i] == 'image/jpeg'){
     $imagem_original = imagecreatefromjpeg($arquivos['tmp_name'][$i]);
     redimensiona($imagem_final, $imagem_original,$largura, $altura, $largura_original, $altura_original,$i, 'jpeg');
-    //marca("imagens/'.$i.'.jpeg", $largura, $altura, $i);
     echo '<img src="imagens/'.$i.'watermark.jpeg"><br><br>';
-    echo '<a href="imagens/'.$i.'watermark.jpeg" download><button>Download</button></a>';
-    echo '<a href="imagens/'.$i.'.jpeg" download><button>Download Sem Marca D\'água</button></a><br><br>';
+    echo '<a href="imagens/'.$i.'watermark.jpeg" download><button type="button" class="btn btn-success">Download <i class="fas fa-file-download"></i></button></a>';
+    echo ' <a href="imagens/'.$i.'.jpeg" download><button type="button" class="btn btn-primary">Download Sem Marca D\'água <i class="fas fa-file-download"></i></button></a><br><br>';
+    echo '<hr>';
   } else {
     $imagem_original = imagecreatefrompng($arquivos['tmp_name'][$i]);
     redimensiona($imagem_final, $imagem_original,$largura, $altura, $largura_original, $altura_original, $i, 'png');
-    //imagepng($imagem_final, 'imagens/'.$i.'.png');
     echo '<img src="imagens/'.$i.'watermark.png"><br><br>';
-    echo '<a href="imagens/'.$i.'watermark.png" download><button>Download</button></a>';
-    echo '<a href="imagens/'.$i.'.png" download><button>Download Sem Marca D\'água</button></a><br><br>';
+    echo '<a href="imagens/'.$i.'watermark.png" download><button class="btn btn-success">Download <i class="fas fa-file-download"></i></button></a>';
+    echo ' <a href="imagens/'.$i.'.png" download><button type="button" class="btn btn-primary">Download Sem Marca D\'água <i class="fas fa-file-download"></i></button></a><br><br>';
+    echo '<hr>';
   }
+  ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
 
 }
 
+require_once 'rodape.html';
 ?>
